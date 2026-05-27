acbf5e56fc7bab389d057aa0636bfbab8120c6ae
kwilson44 <113068847+kwilson44@users.noreply.github.com>
2026-05-27 14:25:57 -0400

    Add PWA support: manifest, service worker, apple icon, theme color

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'elizabeth-v1';
+const urlsToCache = [
+  '/elizabeth_wilson_education/',
+  '/elizabeth_wilson_education/index.html',
+  '/elizabeth_wilson_education/favicon.ico',
+  '/elizabeth_wilson_education/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type === 'error') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      }).catch(() => {
+        return caches.match('/elizabeth_wilson_education/index.html');
+      });
+    })
+  );
+});
